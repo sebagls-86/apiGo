c34277d7d47a3f89948eac05f02898e994213688
Seba <63512521+sebagls-86@users.noreply.github.com>
2022-04-08 13:44:59 -0300

    subida inicial

---
+++ services/user.service.impl.go
@@ -0,0 +1,52 @@
+package services
+
+import (
+	"api-go/models"
+	"context"
+	"errors"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type UserServiceImpl struct {
+	usercollection *mongo.Collection
+	ctx            context.Context
+}
+
+func NewUserService(usercollection *mongo.Collection, ctx context.Context) UserService {
+	return &UserServiceImpl{
+		usercollection: usercollection,
+		ctx:            ctx,
+	}
+}
+
+func (u *UserServiceImpl) CreateUser(user *models.User) error {
+	_, err := u.usercollection.InsertOne(u.ctx, user)
+	return err
+}
+
+func (u *UserServiceImpl) GetUser(name *string) (*models.User, error) {
+	var user *models.User
+	query := bson.D{bson.E{Key: "user_name", Value: name}}
+	err := u.usercollection.FindOne(u.ctx, query).Decode(&user)
+	return user, err
+}
+
+func (u *UserServiceImpl) GetAll() ([]*models.User, error) {
+	return nil, nil
+}
+
+func (u *UserServiceImpl) UpdateUser(user *models.User) error {
+	filter := bson.D{bson.E{Key: "user_name", Value: user.Name}}
+	update := bson.D{bson.E{Key: "$set", Value: bson.D{bson.E{Key: "user_name", Value: user.Name}, bson.E{Key: "user_age", Value: user.Age}, bson.E{Key: "user_address", Value: user.Address}}}}
+	result, _ := u.usercollection.UpdateOne(u.ctx, filter, update)
+	if result.MatchedCount != 1 {
+		return errors.New("no matched documento found for update")
+	}
+	return nil
+}
+
+func (u *UserServiceImpl) DeleteUser(name *string) error {
+	return nil
+}
